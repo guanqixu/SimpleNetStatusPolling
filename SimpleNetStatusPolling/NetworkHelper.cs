6c68366d1d25d26662acd52886024c9a8150d848
guanqixu <guanqixu@qq.com>
2020-01-13 15:53:36 +0800

    调成为 .net 4.0 的版本

--- SimpleNetStatusPolling/NetworkHelper.cs
+++ SimpleNetStatusPolling/NetworkHelper.cs
@@ -4,38 +4,25 @@ using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace SimpleNetStatusPolling
 {
     class NetworkHelper
     {
-
-
         public static bool IsOnline(string ipAddress, int port, int timeout)
         {
             TcpClient client = new TcpClient();
-            var ipe = new IPEndPoint(IPAddress.Parse(ipAddress), port);
-            var task = client.ConnectAsync(IPAddress.Parse(ipAddress), port);
-            task.Wait(timeout);
-
-            var result = false;
-            result = client.Connected;
+            var ar = client.BeginConnect(ipAddress, port, null, null);
+            ar.AsyncWaitHandle.WaitOne(timeout);
+            var result = client.Connected;
             client.Close();
-
             return result;
         }
 
         public static bool IsOnline(IPEndPoint ipe, int timeout)
         {
-            TcpClient client = new TcpClient();
-            var task = client.ConnectAsync(ipe.Address, ipe.Port);
-            task.Wait(timeout);
-
-            var result = false;
-            result = client.Connected;
-            client.Close();
-
-            return result;
+            return IsOnline(ipe.Address.ToString(), ipe.Port, timeout);
         }
 
     }
